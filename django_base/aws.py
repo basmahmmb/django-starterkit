8d1cd7b63291a4a1e6ab59bf84d39301aa8fbc2e
ahmadiga <a.bazadough@sit-mena.com>
2015-12-15 02:38:31 +0200

    first commit

---
+++ django_base/aws.py
@@ -0,0 +1,30 @@
+
+try:
+    from settings import *
+except ImportError:
+    pass
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "molPortal/statics"),
+)
